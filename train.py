f6475b21660ca6977d7bb5f1032c89592db30a0e
Shintaro Takagi <shintaro.good@gmail.com>
2019-01-04 13:26:33 +0900

    cross validation

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import load as ld
 import os as os
 from logging import StreamHandler,DEBUG,Formatter,FileHandler,getLogger
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import log_loss,roc_auc_score,auc
 
 logger = getLogger(__name__)
 DIR = 'log/'
@@ -28,7 +30,7 @@ if __name__ == '__main__':
     df = ld.load_train_csv()
     
     x_train = df.drop('target',axis=1)
-    y_train = df['target'].values
+    y_train = df['target']
     
     use_cols = x_train.columns.values
     logger.debug('train columns {} {}'.format(use_cols.shape,use_cols))
@@ -36,20 +38,37 @@ if __name__ == '__main__':
     
     logger.info('preparation end')
     
-    clf = LogisticRegression(random_state=42)
-    clf.fit(x_train.values,y_train)
+    cv = StratifiedKFold(n_splits=5,shuffle=True,random_state=42)
     
+    #cross validation
+    for train_idx,valid_idx in cv.split(x_train,y_train):
+        
+        trn_x = x_train.iloc[train_idx,:]
+        val_x = x_train.iloc[valid_idx,:]
+        
+        trn_y = y_train[train_idx]
+        val_y = y_train[valid_idx]
+        
+        #logstic regression fit
+        clf = LogisticRegression(random_state=42)
+        clf.fit(trn_x.values,trn_y.values)
+        pred = clf.predict_proba(val_x)[:,1]
+        sc_logloss = log_loss(val_y,pred)
+        sc_auc = roc_auc_score(val_y,pred)
+        
+        logger.info('logloss: {},auc: {}'.format(sc_logloss,sc_auc))
+        
     logger.info('train end')
     
     df = ld.load_test_csv()
-    
+
     x_test = df[use_cols].sort_values('id')
-    
+
     logger.debug('test matrrix {}'.format(x_test.shape))
     pred_test = clf.predict_proba(x_test.values)
-    
+
     df_submit = pd.read_csv(SAMPLE_SUBMIT_FILE).sort_values('id')
     df_submit['target'] = pred_test
     df_submit.to_csv(os.path.join(DIR,'sample.csv'),index=False)
-    
-    logger.info('all end')
+
+    logger.info('all end')    
